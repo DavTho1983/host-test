a0d33e3ccb5eb34a6ff0db24f0b61e273ae4b848
DavTho1983 <david.alex.thompson@gmail.com>
2020-09-21 12:27:34 +0100

    commit to test push trigger 31

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         Hello CI/CD with Google Cloud Build!
-        Version 30?
+        Version 31?
       </header>
     </div>
   );
