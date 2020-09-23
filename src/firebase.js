2526e546b1a2aaf5eac8096927f69e60389c8b89
DavTho1983 <david.alex.thompson@gmail.com>
2020-09-23 17:34:57 +0100

    production config test KMS ENCRYPT

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,8 @@ import * as firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = firebase.functions.config()
 const firebaseConfig = {
-	apiKey: config.myconfig.apikey,
+	apiKey: "AIzaSyDuA3EiNMF9YRbu278LRQcUUYYtJqAu2Vk",
     // authDomain: "ammonite-prototype-2dfa7.firebaseapp.com",
     // databaseURL: "https://ammonite-prototype-2dfa7.firebaseio.com",
     projectId: "host-test-4cd54",
