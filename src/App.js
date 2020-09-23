c44bee6b35b66036a8ba7d824bb78447a9ea0e25
DavTho1983 <david.alex.thompson@gmail.com>
2020-09-23 11:01:53 +0100

    test for CI/CD with separate projects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         Hello CI/CD with Google Cloud Build!
-        PRODUCTION TEST!!!!!!!!!!!!!!!!!!!!!
+        PRODUCTION TEST!!!!!!!!!!!!!!!!!!!!!!
       </header>
     </div>
   );
